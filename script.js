36a5390149e6baa3cca43c0f231482c8a872f502
ariadna <gomezariadna.ag@gmail.com>
2017-03-06 22:49:30 +0000

    mileston 2 completed

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// JavaScript File
+$(document).ready(function(){
+    
+    $("button").click(function(){
+        var person = $("#password").val();
+     
+        if (person === "22") {
+            $("div").append("CONGRATULATIONS,YOU GUESSED IT!!!");
+        }
+    });
+});
