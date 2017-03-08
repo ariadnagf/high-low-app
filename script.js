f30382891de53f50d2807a83a13bf5ad68d9c320
ariadna <gomezariadna.ag@gmail.com>
2017-03-08 22:22:38 +0000

    milestone 3 completed

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
-    
+    var answer=22;
     $("button").click(function(){
         var person = $("#password").val();
-     
-        if (person === "22") {
+        person=parseInt(person)
+     if (person>answer) {
+         $("div").append("the answer is too high");
+     }
+         else if (person<answer) {
+         $("div").append("the answer is too low");
+     }
+        else if (person === answer) {
             $("div").append("CONGRATULATIONS,YOU GUESSED IT!!!");
+        }
+        else{
+           $("div").append("what you typed is not a number."); 
         }
     });
 });
+
